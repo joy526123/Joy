ebf6c6aecc415093992bb818294d12957d68d522
Joy <62205385+joy526123@users.noreply.github.com>
2025-08-11 20:38:22 +0800

    Update liff-init.js

--- liff-init.js
+++ liff-init.js
@@ -1,1 +1,12 @@
-71fe327/liff-init.js
+var liffID = 'XXXXXXXXXXXX';
+
+liff.init({
+  liffId: liffID
+}).then(function() {
+  console.log('LIFF init');
+  
+  // 這邊開始寫使用其他功能
+  
+}).catch(function(error) {
+  console.log(error);
+});
